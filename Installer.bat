e315faecbb4a402d8b8ca98fe41de2d71cc83f04
Nclear <xyz.games.apps@gmail.com>
2023-04-06 22:24:37 +0200

    Update Installer.bat

--- Installer.bat
+++ Installer.bat
@@ -1,7 +1,7 @@
 msg * Make sure Windows Desktop Gadgets is installed on this computer.
 if exist DigitalClock.gadget\ (
   move "DigitalClock.gadget" "%localappdata%\Microsoft\Windows Sidebar\Gadgets\"
-  msg * Digital Clock Widget installed.
+  msg * Digital Clock Widget is now installed and available in Widget Gallery.
 ) else (
-  msg * Couldn't find the installation files, make sure to select 'Download ZIP' in GitHub and then unpack files.
+  msg * Couldn't find the installation files, make sure to select 'Download ZIP' on GitHub and then unpack files.
 )
